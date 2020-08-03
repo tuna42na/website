84aa8b040a2e9b1dce863217c42858bbbaa43de5
Tuna Fortuna <tuna42na@gmail.com>
2020-08-03 16:08:19 -0400

    Minimal Design

--- wp_site_1586291552/wp-content/themes/customizr/templates/parts/header/parts/search_form.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * The template for displaying the header search contaienr
- */
-?>
-<<?php czr_fn_echo('element_tag') ?> class="header-search__container <?php czr_fn_echo( 'element_class' ) ?>">
-  <?php get_search_form() ?>
-</<?php czr_fn_echo('element_tag') ?>>
